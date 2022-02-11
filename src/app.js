8a6aa332de8025dda1de3de0e25e6a2ade5f6581
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-11 14:01:29 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 let nodemon = require('nodemon')
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../views/
                 index.html '));
 
             })
