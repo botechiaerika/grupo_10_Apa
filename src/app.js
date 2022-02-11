e36c26793ce15ec0c77a435d8f5a2720e67123f0
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-11 12:59:05 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const path = require('path')
 const resolve = resolve()
 app.use(express.static('/public'))
 
+<<<<<<< Updated upstream
 app.listen(port, () => {
     app.get('/', (req, res) => {
         res.sendFile(path(__dirname, '../views/index.html'))
@@ -15,3 +16,73 @@ app.listen(port, () => {
 
     console.log('express')
 })
+=======
+<<<<<<< Updated upstream
+
+app.listen(3030, function() => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/index.html'));
+    })
+    app.get('/home', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/home.html'));
+    })
+    app.get('/login', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/login.html'));
+    })
+    app.get('/user', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/user.html'));
+    })
+    app.use(express.static(path.resolve(__dirname, '../public/')));
+=======
+
+app.listen(3030, function() => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/index.html'));
+    })
+    app.get('/home', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/home.html'));
+    })
+    app.get('/login', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/login.html'));
+    })
+    app.get('/user', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/user.html'));
+    })
+    app.use(express.static(path.resolve
+(__dirname, '../public/')));
+    console.log("Servidor LEVANTADO");
+});
+const express = require('express') /
+    const app = express()
+let nodemon = nodemon()
+const path = require('path')
+app.use(express.static(public))
+
+
+app.listen(3030, function() => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/index.html'));
+    })
+    app.get('/home', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/home.html'));
+    })
+    app.get('/login', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/login.html'));
+    })
+    app.get('/user', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '
+views/user.html'));
+    })
+    app.use(express.static(path.resolve
+(__dirname, '../public/')));
+>>>>>>> Stashed changes
+    console.log("Servidor LEVANTADO");
+});
+>>>>>>> Stashed changes
