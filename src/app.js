6e52a651de083b64c4cac49bb41332445fcd5865
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-11 07:50:50 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,31 +1,17 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-let nodemon = require('nodemon')
-app.use(express.static('public'));
+const express = require('express')
+const app = express()
+const port = 3030
+const path = require('path')
+const resolve = resolve()
+app.use(express.static('/public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+app.listen(port, () => {
+    app.get('/', (req, res) => {
+        res.sendFile(path(__dirname, '../views/index.html'))
+    });
+    app.get('/public', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '/public/'))
+    });
 
+    console.log('express')
 })
-
-app.get('/manager', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/manager.
-html'));
-});
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/home.html'));
-
-})
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/login.html'));
-})
-
-app.get('/user', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/user.html'));
-})
-app.use(express.static(path.resolve(__dirname, '../public/')));
-
-app.listen(3030, function() {
-    console.log("Servidor LEVANTADO");
-});
