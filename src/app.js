a94d204ceb6a068991da917d42e2e9d50e4e081b
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-11 02:29:37 -0300

    agreegando paginas

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let nodemon = require('nodemon')
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
+
+})
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/home.html'));
+
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/login.html'));
+})
+
+app.get('/user', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/user.html'));
+})
+app.use(express.static(path.resolve(__dirname, '../public/')));
+app.listen(3030, function() {
+    console.log("Servidor LEVANTADO");
+});
