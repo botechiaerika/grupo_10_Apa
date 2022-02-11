4f9f258274c222ab6ee8aff62fb4eae7249d71cb
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-11 13:44:16 -0300

    package

--- src/app.js
+++ src/app.js
@@ -5,36 +5,35 @@ let nodemon = require('nodemon')
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-index.html'));
+            res.sendFile(path.resolve(__dirname, '../views/
+                index.html '));
 
-})
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-home.html'));
+            })
+            app.get('/home', (req, res) => {
+                        res.sendFile(path.resolve(__dirname, '../views/
+                            home.html '));
 
-})
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-login.html'));
-})
+                        })
+                        app.get('/login', (req, res) => {
+                            res.sendFile(path.resolve(__dirname, './views/login.html'));
+                        })
 
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-dashboard.html'));
-})
-app.get('/reserve', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-reserve.html'))
-})
-app.get('/destaques', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-destaques.html'))
-})
-app.use(express.static(path.resolve(__dirname, '../
-public/')));
+                        app.get('/dashboard', (req, res) => {
+                                    res.sendFile(path.resolve(__dirname, '../views/
+                                        dashboard.html '));
+                                    })
+                                    app.get('/reserve', (req, res) => {
+                                                res.sendFile(path.resolve(__dirname, '../views/
+                                                    reserve.html '))
+                                                })
+                                                app.get('/destaques', (req, res) => {
+                                                            res.sendFile(path.resolve(__dirname, '../views/
+                                                                destaques.html '))
+                                                            })
+                                                            app.use(express.static(path.resolve(__dirname, '../
+                                                                            public / ')));
 
 
-app.listen(3030, function() {
-    console.log("Servidor LEVANTADO");
-});
+                                                                            app.listen(3030, function() {
+                                                                                console.log("Servidor LEVANTADO");
+                                                                            });
