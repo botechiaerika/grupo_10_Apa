06b50f18ebb8c61b8751dd2ee936d5ebeaba0bd4
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-12 23:22:47 -0300

    html css home

--- src/app.js
+++ src/app.js
@@ -5,36 +5,22 @@ let nodemon = require('nodemon')
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-index.html'));
-
+    res.sendFile(path.resolve(__dirname, '../views/index.html'));
 })
 app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-home.html'));
+    res.sendFile(path.resolve(__dirname, '../views/home.html'));
 
 })
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-login.html'));
+    res.sendFile(path.resolve(__dirname, '../views/login.html'));
 })
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-dashboard.html'));
+app.get('/reservas', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/productCard.html'));
 })
-app.get('/reserve', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-reserve.html'))
+app.get('/user', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/user.html'));
 })
-app.get('/destaques', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/
-destaques.html'))
-})
-app.use(express.static(path.resolve(__dirname, '../
-public/')));
-
-
-app.listen(3030, function() {
+app.use(express.static(path.resolve(__dirname, '../public/')));
+app.listen(3030, () => {
     console.log("Servidor LEVANTADO");
 });
