6cf5a5eb400e9c589daf4383d26f844b4e3fbf24
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-15 02:49:41 -0300

    home processo final

---
+++ .notes/sprint_02.js
@@ -0,0 +1,7 @@
+let num
+
+let ToDo = (num) => {
+	this.sprint = `Sprint # ${num}`,
+	this.toDO_01 = `{num} `
+	this.td02 =
+}
--- src/app.js
+++ src/app.js
@@ -1,28 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/index.html'));
-})
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/home.html'));
-
-})
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/login.html'));
-})
-app.get('/reservas', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/productCard.html'));
-})
-app.get('/user', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/user.html'));
-})
-app.use(express.static(path.resolve(__dirname, '../public/')));
-app.listen(3030, () => {
-    console.log("Servidor LEVANTADO");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/index.html"));
+});
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/home.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/login.html"));
+});
+app.get("/reservas", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/productCard.html"));
+});
+app.get("/destaques", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/desaques.html"));
+});
+app.get("/sprint02", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/journey/sprint02.html"));
+});
+app.get("/user", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/user.html"));
 });
 
-let nodemon = require('nodemon')
+app.use(express.static(path.resolve(__dirname, "../public/")));
+
+app.listen(3030, () => {
+  console.log("Servidor LEVANTADO");
+});
