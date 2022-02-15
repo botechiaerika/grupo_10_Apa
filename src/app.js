9618f0e62e0a3bc3df528be67a502f99de1cea9d
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-15 09:49:28 -0300

    refactorizando

--- src/app.js
+++ src/app.js
@@ -5,29 +5,29 @@ const path = require("path");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/index.html"));
+    res.sendFile(path.resolve(__dirname, "../views/index.html"));
 });
 app.get("/home", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/home.html"));
+    res.sendFile(path.resolve(__dirname, "../views/home.html"));
 });
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/login.html"));
+    res.sendFile(path.resolve(__dirname, "../views/login.html"));
 });
 app.get("/reservas", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/productCard.html"));
+    res.sendFile(path.resolve(__dirname, "../views/productCart.html"));
 });
 app.get("/destaques", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/desaques.html"));
+    res.sendFile(path.resolve(__dirname, "../views/productDetail.html"));
 });
 app.get("/sprint02", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/journey/sprint02.html"));
+    res.sendFile(path.resolve(__dirname, "../views/journey/sprint02.html"));
 });
 app.get("/user", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/user.html"));
+    res.sendFile(path.resolve(__dirname, "../views/user.html"));
 });
 
 app.use(express.static(path.resolve(__dirname, "../public/")));
 
 app.listen(3030, () => {
-  console.log("Servidor LEVANTADO");
+    console.log("Servidor LEVANTADO");
 });
