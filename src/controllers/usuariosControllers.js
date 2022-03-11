8c1034d1012634c73c3cac129ed5d6cfbc22a7ab
BotechiaErika <97427933+BotechiaErika@users.noreply.github.com>
2022-03-11 09:12:17 -0300

    Add files via upload

---
+++ src/controllers/usuariosControllers.js
@@ -0,0 +1,10 @@
+let controller = {
+    usuariosCover: (req, res) => {},
+    iniciarSeccion: (req, res)=> {},
+    miCuenta: (req, res)=> {},
+    loginError: (req, res)=> {},
+    crearCuenta: (req, res)=> {},
+    nuevaCuenta: (req, res)=> {},
+}
+
+module.exports = controller
